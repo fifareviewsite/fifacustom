a928fd183f89a8dc279ac3f5e6ac00d25f7ecd95
koootaro <tomoya.sato.7t@gmail.com>
2022-11-08 03:16:31 +0900

    レイアウト_20221108

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -12,17 +12,32 @@
 @include('layout.header')
  
 @section('content')
-    <p>コンテンツ内容が入ります</p>
+<div class="box-parent">
+    <div class="box">
+        <div class="box-title1">CF20連勝達成フォーメーション</div>
+        <div class="text-parent">
+            <div class="box-formation">4-3-1-2</div>
+            <div class="box-regist1">2022/10/21</div>
+        </div>
+        <div class="box-select1"></div>
+        <div class="box-select2"></div>
+        <div class="box-select3"></div>
+        <div class="box-select4"></div>
+        <div class="box-select5"></div>
+        <div class="box-select6"></div>
+    </div>
+</div>
 @endsection
- 
+{{--
 @include('layout.sub')
  
 @section('pageSub')
     <p>個別サイドバーの内容</p>
 @endsection
- 
+--}}
 @section('pageJs')
 <script src="/js/page.js"></script>
 @endsection
- 
+{{--
 @include('layout.footer')
+--}}
